e964d5a83408bfdda01899049844dfb7d2c982b3
Sebastian Valenzuela <svalenzuela.ortiz@gmail.com>
2024-10-18 16:58:10 -0300

    btn Go home

--- src/presentation/screens/setting/SettingScreen.tsx
+++ src/presentation/screens/setting/SettingScreen.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 import { globalStyles } from '../../theme/theme'
+import PrimaryButton from '../../components/shared/PrimaryButton';
+import { StackActions, useNavigation } from '@react-navigation/native';
 
 const SettingScreen = () => {
+
+const navigator = useNavigation();
+
   return (
     <View style={globalStyles.constainer}>
         <Text>SettingScreen</Text>
+
+        <PrimaryButton
+        label={'Regresar'}
+        onPress={()=> navigator.goBack()}
+        />
+
+        <PrimaryButton
+        label={'Regresar al home'}
+        onPress={()=> navigator.dispatch(StackActions.popToTop)}
+        />
+
     </View>
   )
 }
