189087bf7f354feac40b91970464d020cc45f06d
Sebastian Valenzuela <svalenzuela.ortiz@gmail.com>
2024-10-24 14:37:47 -0300

    drawer

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { NavigationContainer } from '@react-navigation/native'
 import { StackNavigator } from './presentation/routes/StackNavigator';
 import React from 'react'
 import { Text, View } from 'react-native'
+import { SideMenuNavigator } from './presentation/routes/SideMenuNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
       {/* <Text>App.tsx</Text> */}
-      <StackNavigator></StackNavigator>
+      {/* <StackNavigator></StackNavigator> */}
+      <SideMenuNavigator/>
     </NavigationContainer>
   )
 }
